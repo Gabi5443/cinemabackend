89e4a9c5c81ebbbcaa224603e43643bf608ec1d2
Berbet <juliatrindadeberbet@gmail.com>
2026-09-17 09:34:36 -0300

    Update Usuario.cs

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -3,9 +3,9 @@ namespace cinemabackend.Models;
 public class Usuario {
         private int Id {get; private set;}
         private string Name {get; private set;}
-        private string email {get; private set;}
-        private string cpf {get; private set;}
-        private string senha {get; private set;}
+        private string Email {get; private set;}
+        private string Cpf {get; private set;}
+        private string Senha {get; private set;}
        
 
         public Usuario (int id, string name){
