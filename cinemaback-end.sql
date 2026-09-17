605460bc3c6740434bfc104d9a30f9a05ca13f2f
Berbet <juliatrindadeberbet@gmail.com>
2026-09-17 09:05:18 -0300

    Update cinemaback-end.sql

--- cinemaback-end.sql
+++ cinemaback-end.sql
@@ -8,7 +8,7 @@ USE CINEMA;
 
 
 CREATE TABLE LOGRADOURO (
-    ID_LOGRADOURO INT PRIMARY KEY,
+    ID_LOGRADOURO INT PRIMARY KEY AUTO_INCREMENT,
     RUA VARCHAR (100),
     BAIRRO VARCHAR (100),
     CIDADE VARCHAR (100),
@@ -20,7 +20,7 @@ CREATE TABLE LOGRADOURO (
 
 
 CREATE TABLE USUARIO(
-    ID_USUARIO INT PRIMARY KEY,
+    ID_USUARIO INT PRIMARY KEY AUTO_INCREMENT,
     CPF_USUARIO VARCHAR (20),
     NOME_USUARIO VARCHAR(100),
     EMAIL_USUARIO VARCHAR (100),
@@ -30,7 +30,7 @@ CREATE TABLE USUARIO(
 );
 
  CREATE TABLE CATEGORIA (
-        ID_CATEGORIA INT PRIMARY KEY,
+        ID_CATEGORIA INT PRIMARY KEY AUTO_INCREMENT,
         NOME_CATEGORIA VARCHAR (100),
         DESCRICAO_CATEGORIA TEXT
 );
@@ -38,7 +38,7 @@ CREATE TABLE USUARIO(
 
 CREATE TABLE FILME (
     TITULO_FILME VARCHAR (100),
-    ID_FILME INT PRIMARY KEY,
+    ID_FILME INT PRIMARY KEY AUTO_INCREMENT,
     DESCRICAO_FILME TEXT,
     SINOPSE_FILME TEXT,
     SUBTITULO_FILME VARCHAR (100),
@@ -51,7 +51,7 @@ CREATE TABLE FILME (
 
 
 CREATE TABLE INGRESSO (
-     ID_INGRESSO INT PRIMARY KEY,
+     ID_INGRESSO INT PRIMARY KEY AUTO_INCREMENT,
      PRECO_INGRESSO DECIMAL(10,2),
      ID_FILME_FK INT,
      FOREIGN KEY (ID_FILME_FK) REFERENCES FILME (ID_FILME),
@@ -62,7 +62,7 @@ CREATE TABLE INGRESSO (
 );
 
 CREATE TABLE CARRINHO (
-    iD_CARRINHO INT PRIMARY KEY,
+    iD_CARRINHO INT PRIMARY KEY AUTO_INCREMENT,
     ID_INGRESSO_FK INT,
     FOREIGN KEY (ID_INGRESSO_FK) REFERENCES INGRESSO (ID_INGRESSO),
     FORMA_PAGAMENTO TEXT,
@@ -70,4 +70,17 @@ CREATE TABLE CARRINHO (
     STATUS ENUM('EM ANDAMENTO', 'PAGAMENTO CONCLUÍDO', 'PAGAMENTO CANCELADO') DEFAULT 'EM ANDAMENTO',
     HORA_PAGAMENTO TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 
+
 );
+
+
+INSERT INTO CATEGORIA (NOME_CATEGORIA, DESCRICAO_CATEGORIA) VALUES 
+('AÇÃO', 'Foco em dinamismo, perseguições, lutas, explosões e ritmo acelerado. O herói geralmente enfrenta grandes perigos para vencer um antagonista ou superar uma crise iminente.'), 
+('TERROR', 'Destinado a amedrontar, chocar ou causar repulsa no espectador, explorando o medo do desconhecido, da morte ou de forças sobrenaturais.'), 
+('AVENTURA', 'Narrativas centradas em jornadas épicas, exploração de territórios desconhecidos e busca por objetivos grandiosos ou tesouros.'), 
+('FANTASIA', 'Histórias ambientadas em universos imaginários que rompem as leis da física e da realidade conhecida.'), 
+('DRAMA', 'Foco no desenvolvimento profundo de personagens, em conflitos emocionais intensos e em situações humanas realistas ou comoventes.'), 
+('CRIMINAL', 'Histórias centradas na prática de delitos, investigações de crimes, julgamentos ou na vida de criminosos e detetives.');
+
+
+
