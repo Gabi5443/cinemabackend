0a191fdcdb89144eca34fd92de76234bf1d1d5a4
Berbet <juliatrindadeberbet@gmail.com>
2026-09-19 23:28:48 -0300

    Update Filme.cs

--- Models/Filme.cs
+++ Models/Filme.cs
@@ -3,12 +3,12 @@ namespace cinemabackend.Models;
 public class Filme 
 { 
 
-    public string Titulo { get; private set; } 
-    public int Id { get; private set; } 
-    public string Descricao { get; private set; } 
-    public string Sinopse { get; private set; } 
-    public TimeOnly Duracao { get; private set; } 
-    public string Subtitulo { get; private set; } 
+    private string Titulo { get; private set; } 
+    private int Id { get; private set; } 
+    private string Descricao { get; private set; } 
+    private string Sinopse { get; private set; } 
+    private TimeOnly Duracao { get; private set; } 
+    private string Subtitulo { get; private set; } 
 
     public Filme(string titulo, int id, string descricao, string sinopse, TimeOnly duracao, string subtitulo) 
     { 
