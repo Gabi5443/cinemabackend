5a48047be813bd995baebb97009959514d3d306e
Berbet <juliatrindadeberbet@gmail.com>
2026-09-20 22:17:22 -0300

    Atualizar o ingresso.cs

--- Models/ingresso.cs
+++ Models/ingresso.cs
@@ -5,13 +5,15 @@ public class Ingresso
   public int Id_ingresso {get; private set;}
   public decimal Preco_ingresso {get; private set;}
   public Usuario Usuario {get; private set;}
+  public Filme filme {get; private set;}
 
 
-  public Ingresso (int id_ingresso, decimal preco_ingresso, Usuario usuario)
+  public Ingresso (int id_ingresso, decimal preco_ingresso, Usuario usuario, Filme filme)
   {
     Id_ingresso = id_ingresso;
     Preco_ingresso = preco_ingresso;
     Usuario = usuario;
+    Filme = filme;
     
   }
 }
