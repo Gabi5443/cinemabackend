83fe5cd5bbe73d28a61e2071d9dfcce9a816ce2c
Berbet <juliatrindadeberbet@gmail.com>
2026-09-21 09:10:22 -0300

    Update carrinhoRequest.cs

--- DTOs/carrinhoRequest.cs
+++ DTOs/carrinhoRequest.cs
@@ -1,1 +1,10 @@
 
+namespace cinemabackend.DTOs;
+
+public class CarrinhoRequest
+{
+    public int IdIngressoFk { get; set; }
+    public string FormaPagamento { get; set; }
+    public double Desconto { get; set; }
+    public string Status { get; set; }
+}
