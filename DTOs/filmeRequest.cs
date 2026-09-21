09474b879fe097df8f70469aef443b0bbd1c67cd
Gabi5443 <gabrielatablet8@gmail.com>
2026-09-21 09:10:42 -0300

    Update filmeRequest.cs

--- DTOs/filmeRequest.cs
+++ DTOs/filmeRequest.cs
@@ -1,1 +1,11 @@
+namespace cinemabackend.DTOs;
 
+public class FilmeRequest
+{
+    public string TituloFilme { get; set; }
+    public string DescricaoFilme { get; set; }
+    public string SinopseFilme { get; set; }
+    public string SubtituloFilme { get; set; }
+    public TimeSpan DuracaoFilme { get; set; }
+    public int IdCategoriaFk { get; set; }
+}
