61610ff47df1b11a170af069765d908280c6d579
Berbet <juliatrindadeberbet@gmail.com>
2026-09-20 22:26:18 -0300

    Atualizar o Carrinho.cs

--- Models/Carrinho.cs
+++ Models/Carrinho.cs
@@ -7,15 +7,17 @@ public class Carrinho
   public decimal Desconto {get; private set;}
   public string Status {get; private set;}
   public TimeOnly Hora_pagamento {get; private set;}
+  public Ingresso ingresso {get;private set;}
 
 
-  public Carrinho (int id_carrinho, string forma_pagamento, decimal desconto, string status, TymeOnly hora_pagamento )
+  public Carrinho (int id_carrinho, string forma_pagamento, decimal desconto, string status, TymeOnly hora_pagamento, Ingresso ingresso)
   {
     Id_carrinho = id_carrinho;
     Forma_pagamento = forma_pagamento;
     Desconto = desconto;
     Status = status;
     Hora_pagamento = hora_pagamento;
+    Ingresso = ingresso;
     
   }
   
