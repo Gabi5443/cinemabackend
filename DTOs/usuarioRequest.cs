bd0e5ec3e4ae64f193657f489d646186dbab8474
Gabi5443 <gabrielatablet8@gmail.com>
2026-09-21 09:24:14 -0300

    Update usuarioRequest.cs

--- DTOs/usuarioRequest.cs
+++ DTOs/usuarioRequest.cs
@@ -1,9 +1,10 @@
 namespace cinemabackend.DTOs;
 
-public record UsuarioRequest(
+public record UsuarioRequest
+{
     public string CpfUsuario { get; set; }
     public string NomeUsuario { get; set; }
     public string EmailUsuario { get; set; }
     public int IdLogradouroFk { get; set; }
     public string SenhaUsuario { get; set; }
-);
+};
