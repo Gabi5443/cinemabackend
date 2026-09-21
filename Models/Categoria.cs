13f4f125f258180851f026888568bdd98f222005
Berbet <juliatrindadeberbet@gmail.com>
2026-09-20 21:04:15 -0300

    Update Categoria.cs

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,1 +1,17 @@
+namespace cinemabackend.Models;
 
+public class Categoria 
+{ 
+  private string Nome_categoria {get; private set;}
+  private int Id_categoria {get; private set;}
+  private string Descricao_categoria {get; private set;}
+
+  public Categoria (string nome_categoria , int id_categoria, string descricao_categoria)
+  {
+    Nome_categoria = nome_categoria;
+    Id_categoria = id_categoria;
+    Descricao_categoria = Descricao_categoria;
+    
+    
+  }
+}
