d1dc1d9d8de2555c084dd1cf88c2ef46abd61c68
Berbet <juliatrindadeberbet@gmail.com>
2026-09-20 22:20:19 -0300

    Atualizar o Usuario.cs

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -6,13 +6,15 @@ public class Usuario {
         public string Email {get; private set;}
         public string Cpf {get; private set;}
         public string Senha {get; private set;}
+        public Logradouro Logradouro {get; private set;}
        
 
-        public Usuario (int id, string name){
+        public Usuario (int id, string name, string email, string cpf, string senha, Logradouro logradouro){
             Id = id;
             Name = name;
             Email = email;
             Cpf = cpf;
             Senha = senha;
+            Logradouro = logradouro;
         }
 }
