948aac7d3858c1ba832e00c90dcf0901f90c358e
Berbet <juliatrindadeberbet@gmail.com>
2026-09-21 09:06:26 -0300

    Update logradouroRequest.cs

--- DTOs/logradouroRequest.cs
+++ DTOs/logradouroRequest.cs
@@ -1,1 +1,12 @@
 
+namespace cinemabackend.DTOs;
+
+public class LogradouroRequest
+{
+    public string Rua { get; set; }
+    public string Bairro { get; set; }
+    public string Cidade { get; set; }
+    public string Cep { get; set; }
+    public string Descricao { get; set; }
+    public string NumeroCasa { get; set; }
+}
